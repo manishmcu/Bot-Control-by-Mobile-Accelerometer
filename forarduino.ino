d5e0a041b5a591c7efd33b935d4d80a4e253e8f1
Manish Pakhira <48309293+manishmcu@users.noreply.github.com>
2020-01-17 01:00:42 +0530

    Add files via upload

---
+++ forarduino.ino
@@ -0,0 +1,21 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,INPUT);
+pinMode(4,INPUT);
+pinMode(5,OUTPUT);
+pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(2))
+{digitalWrite(5,HIGH);}
+if(!digitalRead(2))
+{digitalWrite(5,LOW);}
+if(digitalRead(4))
+{digitalWrite(7,HIGH);}
+if(!digitalRead(4))
+{digitalWrite(7,LOW);}
+}
